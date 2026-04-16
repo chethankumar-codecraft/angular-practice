21fcba67676ee37f05f96aaac1eb8c38c41e50bb
Chethan Kumar <chethan.kumar@codecraft.co.in>
2026-04-16 06:21:04 +0000

    mock provider

--- src/app/components/counter/counter.ts
+++ src/app/components/counter/counter.ts
@@ -3,37 +3,20 @@ import { Component, signal } from '@angular/core';
 @Component({
   selector: 'app-counter',
   imports: [],
-  // templateUrl: './counter.html',
-  template: `
-    <section>
-      @if (this.isEdit()) {
-        <input onfocus = "this.select()" (change)="handleInputChange($event)" [value]="value()" />
-      } @else {
-        <button (click)="handleInputChange($event)">{{ this.value() }}</button>
-      }
-      <button (click)="increment()">+</button>
-      <button (click)="decrement()">-</button>
-    </section>
-  `,
+  templateUrl: './counter.html',
   styleUrl: './counter.css',
 })
 export class Counter {
-  isEdit = signal(false);
   value = signal(0);
   increment() {
     this.value.update((value) => value + 1);
-    this.isEdit.set(false);
   }
 
   decrement() {
     this.value.update((value) => value - 1);
-    this.isEdit.set(false);
   }
 
   handleInputChange(event: any) {
-    if (event.target.tagName === 'INPUT') {
-      this.value.set(Number(event.target.value));
-    }
-    this.isEdit.update((value) => !value);
+    this.value.set(Number(event.target.value));
   }
 }
--- src/app/components/housing-location/housing-location.ts
+++ src/app/components/housing-location/housing-location.ts
@@ -1,15 +1,18 @@
-import { Component, input, output } from '@angular/core';
+import { Component, inject, input, output } from '@angular/core';
 import { HousingLocationInfo } from '../../models/housing-location-info';
+import { LocationService } from '../../services/location-service';
 
 @Component({
   selector: 'app-housing-location',
   imports: [],
   templateUrl: './housing-location.html',
   styleUrl: './housing-location.css',
+  providers: [{ provide: LocationService, useClass: LocationService }],
 })
 export class HousingLocation {
   location = input.required<HousingLocationInfo>();
   onLocationClick = output<HousingLocationInfo>();
+  locationService = inject(LocationService);
 
   handleClick(event: MouseEvent) {
     console.log(event.target);
