750bfae691e4f8fd4d4b331984ed4aad6cc9d2ec
Chethan Kumar <chethan.kumar@codecraft.co.in>
2026-04-21 11:20:49 +0000

    Implemented linked signal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { Home } from '@components/home/home';
+import { LinkedSignalDemo } from '@components/linked-signal-demo/linked-signal-demo';
 import { LocationDetails } from '@components/location-details/location-details';
 
 export const routes: Routes = [
@@ -12,5 +13,10 @@ export const routes: Routes = [
     path: 'details/:id',
     component: LocationDetails,
     title: 'Home details',
+  },
+  {
+    path: 'linked-signal',
+    component: LinkedSignalDemo,
+    title: 'Linked Signal',
   },
 ];
---
+++ src/app/components/linked-signal-demo/linked-signal-demo.ts
@@ -0,0 +1,73 @@
+import { Component } from '@angular/core';
+import { signal, computed, linkedSignal } from '@angular/core';
+
+@Component({
+  selector: 'app-linked-signal-demo',
+  templateUrl: './linked-signal-demo.html',
+  styleUrl: './linked-signal-demo.css',
+})
+export class LinkedSignalDemo {
+  userStatus = signal<'online' | 'away' | 'offline'>('offline');
+
+  // TODO: Create notificationsEnabled computed signal that returns true when status is 'online'
+  // notificationEnabled = computed(() => {
+  //   return this.userStatus() === 'online';
+  // });
+  notificationEnabled = linkedSignal(() => {
+    return this.userStatus() === 'online';
+  });
+
+  // TODO: Create statusMessage computed signal that returns appropriate message for each status
+  statusMessage = computed(() => {
+    const status = this.userStatus();
+    switch (status) {
+      case 'online':
+        return 'Available for meetings and messages';
+      case 'away':
+        return 'Temporarily away, will respond soon';
+      case 'offline':
+        return 'Not available, check back later';
+      default:
+        return 'Status unknown';
+    }
+  });
+
+  // TODO: Create isWithinWorkingHours computed signal that calculates if user is within working hours
+
+  isWithinWorkingHours = computed(() => {
+    const now = new Date();
+    const hour = now.getHours();
+    const isWeekday = now.getDay() > 0 && now.getDay() < 6;
+    return isWeekday && hour >= 9 && hour < 17 && this.userStatus() !== 'offline';
+  });
+  goOnline() {
+    this.userStatus.set('online');
+  }
+
+  goAway() {
+    this.userStatus.set('away');
+  }
+
+  goOffline() {
+    this.userStatus.set('offline');
+  }
+
+  toggleStatus() {
+    const current = this.userStatus();
+    switch (current) {
+      case 'offline':
+        this.userStatus.set('online');
+        break;
+      case 'online':
+        this.userStatus.set('away');
+        break;
+      case 'away':
+        this.userStatus.set('offline');
+        break;
+    }
+  }
+  toggleNotifications() {
+    // This works with linkedSignal but would error with computed!
+    this.notificationEnabled.update((prev) => !prev);
+  }
+}
